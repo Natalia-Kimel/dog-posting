905e6017b9fec6727c61e92096f3ddce9f7cee98
Natalia Kimel <nata@rus9n.com>
2024-12-11 18:48:19 +0300

    fix: router in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductPage from './components/ProductPage';
 import CreateProduct from './components/CreateProduct';
